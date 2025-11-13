6124e46b118a6bf8d5f6246f597faefda2e8d80a
Davvii1 <david2002rg@hotmail.com>
2025-11-13 13:54:00 -0600

    style: prettier and eslint configured

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,17 +4,13 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
   features: {
+    typescript: true,
     // Rules for module authors
     tooling: true,
     // Rules for formatting
     stylistic: true,
   },
   dirs: {
-    src: [
-      './playground',
-    ],
+    src: ['./playground'],
   },
-})
-  .append(
-    // your custom flat config here...
-  )
+}).append()
