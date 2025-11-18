29216f29e8d6f1325f44803fa84d0fd4f68e1c81
Davvii1 <david2002rg@hotmail.com>
2025-11-18 11:19:04 -0600

    feat(elements): add filters modal with defaults for data pages

---
+++ src/runtime/components/FiltersModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <UModal v-bind="modalProps">
+    <UButton
+      v-bind="buttonProps"
+    />
+
+    <template #body>
+      <slot name="body">
+        <slot />
+      </slot>
+    </template>
+
+    <template #footer>
+      <slot
+        name="footer"
+      >
+        <UButton
+          label="Reset"
+          color="neutral"
+          variant="outline"
+          @click="emit('reset')"
+        />
+        <UButton
+          type="submit"
+          label="Apply"
+          color="neutral"
+        />
+      </slot>
+    </template>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+import type { ModalProps, ButtonProps } from '@nuxt/ui'
+
+withDefaults(defineProps<{
+  modalProps?: ModalProps
+  buttonProps?: ButtonProps
+}>(), {
+  modalProps: () => ({
+    title: 'Filters',
+    scrollable: true,
+    ui: {
+      footer: 'justify-end',
+    },
+  }),
+  buttonProps: () => ({
+    icon: 'i-lucide:filter',
+    label: 'Filters',
+    color: 'neutral',
+    variant: 'solid',
+    class: 'cursor-pointer',
+  }),
+})
+
+const emit = defineEmits(['reset'])
+</script>
